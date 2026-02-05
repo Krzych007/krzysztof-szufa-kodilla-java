a289b47df6cb3bd37b7e60dc19c4bc0db8f98779
Krzych007 <129526426+Krzych007@users.noreply.github.com>
2026-02-05 10:18:21 +0100

    Calc  class added

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,20 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(6, 3);
+
+        System.out.println(sum);
+    }
+}
