4ae48211faec32cacaa3188584302d2410fe8ffa
Krzych007 <129526426+Krzych007@users.noreply.github.com>
2026-02-07 11:58:55 +0100

    6.2 Added calc test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(6, 3);
+
+        if (sum == 6+3){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int sub = calculator.subtract(9, 5);
+
+        if (sub == 9-5){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+    }
 }
