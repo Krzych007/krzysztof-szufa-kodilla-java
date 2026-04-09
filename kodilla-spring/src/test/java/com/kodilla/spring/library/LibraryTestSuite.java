8fea73105e740da5898d061bd551ae5fddae2d17
Krzych007 <129526426+Krzych007@users.noreply.github.com>
2026-04-09 15:15:00 +0200

    13.3 Zadanie: tablica z listami zadań

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootTest
 public class LibraryTestSuite {
 
@@ -34,6 +36,19 @@ public class LibraryTestSuite {
         //Then
         //TODO:
 
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(LibraryConfig.class);
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 
 }
